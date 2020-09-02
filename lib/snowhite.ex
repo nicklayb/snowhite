ef4de33f21d95ea02286ddcab9dacaef0f9de322
Nicolas Boisvert <nicklay@me.com>
2020-09-01 23:12:39 -0400

    Clean inital snowhite

--- lib/snowhite.ex
+++ lib/snowhite.ex
@@ -2,5 +2,4 @@ defmodule Snowhite do
   use Snowhite.Builder
 
   register_module(:top_right, Snowhite.Modules.Clock, timezone: "America/Toronto", locale: "fr")
-  register_module(:top_left, Snowhite.Modules.Clock, timezone: "America/Toronto", locale: "fr")
 end
