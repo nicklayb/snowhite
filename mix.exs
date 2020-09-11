82e3099c72e1917946990e0c0ceebde08e9962ee
Nicolas Boisvert <nicklay@me.com>
2020-09-11 16:59:04 -0400

    Update mix exs file

--- mix.exs
+++ mix.exs
@@ -1,35 +1,44 @@
 defmodule Snowhite.MixProject do
   use Mix.Project
 
+  @github "https://github.com/nicklayb/snowhite"
+  @description "Smart mirror framework"
+  @version "0.1.0"
   def project do
     [
       app: :snowhite,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      source_url: @github,
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      description: @description,
+      package: package()
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
+  def package do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @github
+      },
+      maintainers: ["Nicolas Boisvert"]
+    ]
+  end
+
   def application do
     [
       extra_applications: [:logger, :runtime_tools]
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:phoenix, "~> 1.5.4"},
@@ -48,12 +57,6 @@ defmodule Snowhite.MixProject do
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
       setup: ["deps.get", "cmd npm install --prefix assets"]
