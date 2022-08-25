eacd4211bc78375206c3815e121e046b67081fba
Nicolas Boisvert <nicklay@me.com>
2022-08-25 11:38:58 -0400

    3.1.2 -

--- priv/js/snowhite.js
+++ priv/js/snowhite.js
@@ -36,8 +36,30 @@ var Snowhite = (() => {
   });
 
   // js/snowhite/hooks/Clock.js
+  var SECOND = 1e3;
+  var padPart = (part) => part.toString().padStart(2, "0");
+  var getDate = () => {
+    const date = new Date();
+    return [date.getHours(), padPart(date.getMinutes()), padPart(date.getSeconds())].join(":");
+  };
   var Clock = {
     mounted() {
+      this.setupTimeout();
+    },
+    updated() {
+      if (this.timer) {
+        clearTimeout(this.timer);
+        this.timer = null;
+        this.setupTimeout();
+      }
+    },
+    handleTick() {
+      this.el.querySelector(".time").innerHTML = getDate();
+      this.setupTimeout();
+    },
+    setupTimeout() {
+      const timer = setTimeout(this.handleTick.bind(this), SECOND);
+      this.timer = timer;
     }
   };
   var Clock_default = Clock;
